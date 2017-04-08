917e5426d4fc474a7a724c140ac0ce1f5d3bfaf7
Murat Bas <alonebasketballer@gmail.com>
2017-04-08 18:45:09 +0300

    Update parabolgenerator.py

--- parabolgenerator.py
+++ parabolgenerator.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 
-data2 = eval(input("A'yi gir: "))
-data = eval(input("B'yi gir: "))
-data1 = eval(input("C'yi gir: "))
+a1 = eval(input("A'yi gir: "))
+b1 = eval(input("B'yi gir: "))
+c1 = eval(input("C'yi gir: "))
 a=[]
 b=[]
-# y=0
-# x=-50
 
-for x in range(-50,50,1):
-    y=data2*x**2+data*x-data1
+
+for x in range(-100,100,1):
+    y=a1*x**2+b1*x-c1
     a.append(x)
     b.append(y)
     #x= x+1
@@ -17,8 +16,8 @@ for x in range(-50,50,1):
 fig= plt.figure()
 axes=fig.add_subplot(111)
 axes.plot(a,b)
-r = data/-2
-k = data2*r**2+data*r+(data1)
+r = -1*(b1/2*a1)
+k = (a1*(r**2))+(b1*r)+(c1)
 plt.plot([r],[k],"ro")
 print(r,k)
 plt.show()
